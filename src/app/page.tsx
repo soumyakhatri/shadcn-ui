75cbf12a3be6ca3a9c209bd60945a365a3fecee8
Soumya Khatri <soumya91rias@gmail.com>
2025-11-12 11:51:55 +0530

    Scrollbar Area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AppAreaCharts from "@/components/AppAreaCharts";
 import AppBarCharts from "@/components/AppBarCharts";
 import CardList from "@/components/CardList";
 import PieCharts from "@/components/PieCharts";
+import ToDoList from "@/components/ToDoList";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarCharts/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Popular Content"/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><PieCharts/></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><ToDoList/></div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppAreaCharts/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Latest Transactions"/></div>
     </div>
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,62 @@
+import { ScrollArea } from "./ui/scroll-area"
+
+function ToDoList() {
+    return (
+        <div>
+            Calendar
+            {/* LIST */}
+            <ScrollArea className="h-[400px] border rounded-md mt-4 overflow-y-auto">
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+              <p>Test</p>
+            </ScrollArea>
+        </div>
+    )
+}
+
+export default ToDoList
